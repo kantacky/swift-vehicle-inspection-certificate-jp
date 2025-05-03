2fd32b720e9c3c7284432bb1b5f03e2a7730ee4d
Kanta Oikawa <self@kantacky.com>
2025-05-03 21:09:59 +0900

    Update platforms

--- Package.swift
+++ Package.swift
@@ -1,21 +1,22 @@
 // swift-tools-version: 6.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "VehicleInspectionCertificateJP",
+    platforms: [
+        .iOS(.v18),
+        .macOS(.v15),
+        .visionOS(.v2),
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VehicleInspectionCertificateJP",
-            targets: ["VehicleInspectionCertificateJP"]),
+            targets: ["VehicleInspectionCertificateJP"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "VehicleInspectionCertificateJP"),
+        .target(name: "VehicleInspectionCertificateJP"),
         .testTarget(
             name: "VehicleInspectionCertificateJPTests",
             dependencies: ["VehicleInspectionCertificateJP"]
