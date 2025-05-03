98b48f63060846956e0c777200561adbea3e6527
Kanta Oikawa <self@kantacky.com>
2025-05-03 20:55:41 +0900

    Add example app

---
+++ Examples/VICJPQRScanner/VICJPQRScanner/VICJPQRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VICJPQRScannerApp.swift
+//  VICJPQRScanner
+//
+//  Created by Kanta Oikawa on 2025/05/03.
+//
+
+import SwiftUI
+
+@main
+struct VICJPQRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Examples/VICJPQRScanner/VICJPQRScannerTests/VICJPQRScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  VICJPQRScannerTests.swift
+//  VICJPQRScannerTests
+//
+//  Created by Kanta Oikawa on 2025/05/03.
+//
+
+import Testing
+@testable import VICJPQRScanner
+
+struct VICJPQRScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
