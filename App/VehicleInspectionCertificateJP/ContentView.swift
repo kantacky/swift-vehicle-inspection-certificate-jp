9e84e479c49cc5d67c669e0c086958d140b0fc2e
Kanta Oikawa <self@kantacky.com>
2025-05-03 20:45:33 +0900

    Add sample App

---
+++ App/VehicleInspectionCertificateJP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VehicleInspectionCertificateJP
+//
+//  Created by Kanta Oikawa on 2025/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ App/VehicleInspectionCertificateJP/VehicleInspectionCertificateJPApp.swift
@@ -0,0 +1,17 @@
+//
+//  VehicleInspectionCertificateJPApp.swift
+//  VehicleInspectionCertificateJP
+//
+//  Created by Kanta Oikawa on 2025/05/03.
+//
+
+import SwiftUI
+
+@main
+struct VehicleInspectionCertificateJPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
